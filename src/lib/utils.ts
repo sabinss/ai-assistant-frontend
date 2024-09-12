d5fb3e3a61cdbf6469ca4291e53714a892fcec55
sabin <sabinshrestha292@gmail.com>
2024-09-12 07:41:50 +0545

    added workflow checkbox

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateRandomId(length = 10) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let result = ""
+  const charactersLength = characters.length
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+
+  return result
+}
+
+// Example usage:
+const randomId = generateRandomId(10) // Generates a random ID with 10 characters
+console.log(randomId)
