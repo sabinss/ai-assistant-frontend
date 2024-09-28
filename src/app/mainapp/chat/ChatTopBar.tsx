fdb06b6530927bb823e5cf28c9419712fb9a8d1c
sabin <sabinshrestha292@gmail.com>
2024-09-28 22:36:04 +0545

    reset session

--- src/app/mainapp/chat/ChatTopBar.tsx
+++ src/app/mainapp/chat/ChatTopBar.tsx
@@ -1,53 +1,66 @@
 "use client"
-import { Info, Phone, Video } from 'lucide-react';
+import { Info, Phone, Video } from "lucide-react"
 import botImage from "@/assets/images/bot.png"
-import Image from "next/image";
-import useNavBarStore from "@/store/store";
-export const TopbarIcons = [{ icon: Info }];
-import { GrAdd } from "react-icons/gr";
-import useAuth from "@/store/user";
-import http from "@/config/http";
+import Image from "next/image"
+import useNavBarStore from "@/store/store"
+export const TopbarIcons = [{ icon: Info }]
+import { GrAdd } from "react-icons/gr"
+import useAuth from "@/store/user"
+import http from "@/config/http"
 
-import usePublicChat from "@/store/public_chat";
+import usePublicChat from "@/store/public_chat"
+import useChatConfig from "@/store/useChatSetting"
 
 export default function ChatTopbar() {
-    const { publicChat, publicChatHeaders, setPublicChatHeaders } = usePublicChat();
-    console.log({ publicChat })
-    const { access_token, setChatSession } = useAuth();
-    const changeSession = async () => {
-        const newSession = Math.floor(Math.random() * 1000).toString();
-        if (publicChat) {
-            let newSession = Math.floor(Math.random() * 9000).toString()
-            localStorage.setItem("chat_session_agile_move", newSession)
-            setPublicChatHeaders({ ...publicChatHeaders, chat_session: newSession })
+  const { publicChat, publicChatHeaders, setPublicChatHeaders } =
+    usePublicChat()
+  const { setSessionId } = useChatConfig()
+  console.log({ publicChat })
+  const { access_token, setChatSession } = useAuth()
+  const changeSession = async () => {
+    setSessionId(null)
+    const newSession = Math.floor(Math.random() * 1000).toString()
+    if (publicChat) {
+      let newSession = Math.floor(Math.random() * 9000).toString()
+      localStorage.setItem("chat_session_agile_move", newSession)
+      setPublicChatHeaders({ ...publicChatHeaders, chat_session: newSession })
+    } else {
+      const res = await http.get(
+        `user/profile/changeSession?session=${newSession}`,
+        {
+          headers: { Authorization: `Bearer ${access_token}` },
         }
-        else {
-            const res = await http.get(`user/profile/changeSession?session=${newSession}`, {
-                headers: { Authorization: `Bearer ${access_token}` },
-            })
-            setChatSession(res?.data?.newSession)
-        }
+      )
+      setChatSession(res?.data?.newSession)
     }
-    const { botName } = useNavBarStore()
-    return (
-        <div className="p-3  w-full bg-muted rounded-md flex flex-col ">
-            <div className='flex justify-between items-center'>
-                <div className="title">
-                    <Image src={botImage} className="rounded-full mr-3 inline" alt="" height={30} width={30} />
-                    <h2 className="font-bold text-xl inline">Chat with {botName}</h2>
-                </div>
-                <div className=" flex cursor-pointer p-2  gap-2 border rounded-md hover:text-black"
-                    onClick={changeSession}
-                >
-                    <GrAdd size={20} />
-                    <p >New Session</p>
-                </div>
-            </div>
+  }
+  const { botName } = useNavBarStore()
+  return (
+    <div className="flex  w-full flex-col rounded-md bg-muted p-3 ">
+      <div className="flex items-center justify-between">
+        <div className="title">
+          <Image
+            src={botImage}
+            className="mr-3 inline rounded-full"
+            alt=""
+            height={30}
+            width={30}
+          />
+          <h2 className="inline text-xl font-bold">Chat with {botName}</h2>
+        </div>
+        <div
+          className=" flex cursor-pointer gap-2  rounded-md border p-2 hover:text-black"
+          onClick={changeSession}
+        >
+          <GrAdd size={20} />
+          <p>New Session</p>
+        </div>
+      </div>
 
-            {/* <div className="flex items-center gap-4 mt-3 w-full">
+      {/* <div className="flex items-center gap-4 mt-3 w-full">
                 <Image src={botImage} className="rounded-full" alt="" height={30} width={30} />
                 {greeting}
             </div> */}
-        </div>
-    )
+    </div>
+  )
 }
