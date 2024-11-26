42351ea18ef0d80a28ed43ec74c12c80bf96189d
sabin <sabinshrestha292@gmail.com>
2024-11-26 21:12:27 +0545

    org token added

--- src/store/organization_customer.tsx
+++ src/store/organization_customer.tsx
@@ -3,17 +3,25 @@ import { create } from "zustand"
 type orgCustomerConfig = {
   orgCustomers: { organization: string; customers: [] }
   loading: boolean
+  orgToken: string
   setOrgCustomers: (data: any) => void
   setLoading: (loading: boolean) => void
+  setOrgToken: (token: string) => void
 }
 
 const useOrgCustomer = create<orgCustomerConfig>((set) => ({
   orgCustomers: { organization: "", customers: [] },
   loading: false,
-
+  orgToken: "",
   setOrgCustomers: (data: any) => set({ orgCustomers: data }),
   // Method to set the mock data
   setLoading: (loading: boolean) => set({ loading }),
+  setOrgToken: (orgToken) => {
+    set((state) => ({
+      ...state,
+      orgToken: orgToken,
+    }))
+  },
 }))
 
 export default useOrgCustomer
