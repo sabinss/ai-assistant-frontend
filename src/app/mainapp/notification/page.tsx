eb674b6c41c4e2823c453c137c830fcfbcbfc737
Sabin  Shrestha <sabin@192.168.101.17>
2025-02-25 22:45:40 +0545

    notification fetch

--- src/app/mainapp/notification/page.tsx
+++ src/app/mainapp/notification/page.tsx
@@ -1,11 +1,36 @@
 "use client"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { CiUser } from "react-icons/ci"
 import NotificationListTable from "./NotificationTable"
 import { Bell } from "lucide-react"
-
+import http from "@/config/http"
+import { toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import useAuth from "@/store/user"
 const Notification = () => {
   const [loading, setLoading] = useState(false)
+  const { access_token } = useAuth()
+  const [notifications, setNotifications] = useState([])
+
+  const fetchOrgNotifications = async () => {
+    try {
+      const res = await http.get("/notification/", {
+        headers: { Authorization: `Bearer ${access_token}` },
+      })
+      console.log("res", res.data)
+      setNotifications(res?.data ?? [])
+    } catch (err) {
+      toast.error("Failed to fetch notifications")
+    }
+  }
+
+  useEffect(() => {
+    async function getNotifications() {
+      await fetchOrgNotifications()
+    }
+    getNotifications()
+  }, [])
+
   return (
     <div className="bg-red h-fit w-full">
       <div className="flex items-center py-5">
@@ -21,7 +46,7 @@ const Notification = () => {
           <div className="h-12 w-12 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
         </div>
       ) : (
-        <NotificationListTable />
+        <NotificationListTable notifications={notifications} />
       )}
     </div>
   )
