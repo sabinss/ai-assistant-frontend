f1d0a935416d7fce4148357a4b9843cc0a4bc703
Suvash Joshi <champsuvash@gmail.com>
2025-10-31 05:05:02 +0000

    chore: update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,9 @@ module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
   trailingComma: "es5",
   tabWidth: 2,
-  semi: false,
+  semi: true,
   singleQuote: false,
-};
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "lf",
+}
