5a2072acf4e988b5feeda3c7de4200664fa088dd
sabin <sabin@noveltytechnology.com>
2026-08-05 22:05:03 +0545

    scope removed

--- src/utility/getGoogleUrl.ts
+++ src/utility/getGoogleUrl.ts
@@ -15,7 +15,7 @@ function getGoogleOAuthURL(orgId: any) {
       "https://www.googleapis.com/auth/gmail.send",
       "https://www.googleapis.com/auth/gmail.readonly",
       "https://www.googleapis.com/auth/gmail.compose",
-      "https://www.googleapis.com/auth/gmail.modify",
+      // "https://www.googleapis.com/auth/gmail.modify",
     ].join(" "),
   }
 
